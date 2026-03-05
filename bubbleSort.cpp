c90c97f62a704c6d4eae48fe08f23438093d1ea9
dhiantiutami-spec <dhiantiutami@gmail.com>
2026-03-05 10:47:18 +0700

    make input from user

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -5,6 +5,9 @@ using namespace std;
 int arr[20]; //Deklarasi variable global array a dengan ukuran 20
 int n; //Deklarasi variable global n untuk menyimpan banyaknya elemen
 
-void input (){  //procedur untuk input
-
+void input (){ //procedur untuk 
+    while (true){
+        cout << "Masukkan banyaknya elemen pada array : "; 
+        cin >> n; //Input dari pengguna
+    }
 }
